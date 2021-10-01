e0ee43c37e6547bb878222cea309a97e8e03514f
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-10-02 00:10:04 +0300

    1.1
    1.1

---
+++ 1.1.py
@@ -0,0 +1,9 @@
+print("Введите два числа")
+x = int(input())
+y = int(input())
+if x > y:
+    print("1-ое число больше 2-ого")
+elif x < y:
+    print("2-ое число больше 1-ого")
+else:
+    print("Числа равны")
