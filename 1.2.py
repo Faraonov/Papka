296d857259d555d7b06c02cd57cb66aeb75b0da3
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-10-02 00:22:10 +0300

    1.2
    1.2

---
+++ 1.2.py
@@ -0,0 +1,10 @@
+print("Введите пять чисел")
+a = int(input())
+b = int(input())
+c = int(input())
+f = int(input())
+d = int(input())
+if f - d == 0:
+    print("Делить на ноль нельзя!")
+else:
+    print((a * b - c) / (f - d))
