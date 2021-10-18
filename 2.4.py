6add0199d2e4a8d86d6dd56f5453b1b8442c484b
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-10-18 20:39:01 +0300

    2.4
    2.4

--- 2.4.py
+++ 2.4.py
@@ -1,10 +1,10 @@
-x = input()
+x = list(input())
 x1 = 0
 x2 = 0
 for i in x:
-    if x[i] == "(":
+    if i == "(":
         x1 +=1
-    elif x[i] == ")":
+    elif i == ")":
         x2 +=1
 if x1 > x2:
     print("Не хватает", x1 - x2, "закр. ск.")
