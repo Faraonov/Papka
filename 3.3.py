783a4dd621ff719d9fbb7e677b5a699f9f5f7cc5
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-11-13 21:49:09 +0300

    3.3
    3.3

---
+++ 3.3.py
@@ -0,0 +1,11 @@
+def z(n):
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(n-2):
+        a = a + b
+        print(a)
+        a , b = b , a
+if z(int(input("Введите число"))) != None:
+    print(z(int(input("Введите число"))))
