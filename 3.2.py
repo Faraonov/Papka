48efcd919538e79a876b5ffb0bec0e30e1ad6dca
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-11-13 09:38:47 +0300

    3.2
    3.2

---
+++ 3.2.py
@@ -0,0 +1,11 @@
+def z(n):
+    if (n < 3) or (n == 12):
+        print("зима")
+    elif (n > 2) and (n < 6):
+        print("весна")
+    elif (n > 5) and (n < 9):
+        print("лето")
+    else:
+        print("осень")
+if z(int(input("Введите число от 1 до 12"))) != None:
+   print(z(int(input("Введите число от 1 до 12"))))
