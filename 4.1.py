13398771059c7c9678babceadcdeccfbfe51d3c7
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-11-23 07:01:04 +0300

    4.1
    4.1

--- 4.1.py
+++ 4.1.py
@@ -1,11 +1,15 @@
 from f31 import z
 
-l = z()
-k = int(input("Введите число"))
-if k >  len(l):
-    print("Перестановок больше, чем элементов")
-elif k == 0:
-    print("Перестановок не будет")
-else:
-    l = l[(len(l) - k):] + l[:(len(l)- k)]
-    print(l)
+
+def m():
+    l = z()
+    k = int(input("Введите число"))
+    if k >  len(l):
+        print("Перестановок больше, чем элементов")
+    elif k == 0:
+        print("Перестановок не будет")
+    else:
+        l = l[(len(l) - k):] + l[:(len(l)- k)]
+        print(l)
+    return 0
+x = m()
