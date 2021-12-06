3bb8c9bc1a044cd9a7c32c58925cc914bfe7e598
Faraonov <91352053+Faraonov@users.noreply.github.com>
2021-12-07 01:21:28 +0300

    5
    5

--- 5.py
+++ 5.py
@@ -3,7 +3,7 @@ from f31 import z
 
 def f(a, x):
     if a == []:
-        return 'Пустой список'
+        return None
     else:
         t = len(a) // 2
         j = len(a) - 1
@@ -17,20 +17,36 @@ def f(a, x):
         if l > j:
             return None
         else:
-            for i in range(t + 1):
+            for i in range(t+1):
                 if a[i] == a[t] and i < t:
                     t = i
             return t
 
 
 b = z()
-b.sort(key = int)
+b.sort(key=int)
 print(b)
 m = int(input("Введите искомый элемент"))
 print(f(b, m))
 
 
 assert f([5, 6, 7], 5) == 0
-assert f([1, 2, 4, 5, 6, 7, 8], 17) is None
-assert f([], 1) == "Пустой список"
-assert f([1, 4, 5, 7, 7, 7, 8], 7) == 3
+assert f([7, 5, 4, 2, 6, 8, 1], 17) is None
+assert f([], 1) is None
+assert f([5, 4, 7, 7, 7, 8, 1], 7) == 2
+assert f([], 42) is None
+assert f([0], 0) == 0
+assert f([0], 1) is None
+assert f([-1, 0, 42], 0) == 1
+assert f([-42, 0, 42], 42) == 2
+assert f([-6, -5, -4, -3, -2, -1], -4) == 2
+assert f([1, 2, 3, 4, 5, 6], 4) == 3
+assert f([1, 2, 3, 4, 5, 6, 7], 4) == 3
+assert f([1, 2, 3, 4, 5], 7) is None
+assert f([1, 2, 3, 4, 5, 6], 7) is None
+assert f([42, 42, 42, 42, 42], 42) == 0
+assert f([-42, -42, -42, -42, -42], -42) == 0
+assert f([42, 42, 42, 42, 43], 43) == 4
+assert f([41, 42, 42, 42, 42], 41) == 0
+assert f([-2, -2, -1, 0, 1, 2, 2, 2], -1) == 2
+assert f([-2, -2, -1, 0, 1, 1, 2, 2], 1) == 4
